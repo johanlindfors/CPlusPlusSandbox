88c734d5fa18595fd928e7e1141338927a214d94
Johan Lindfors <johan.lindfors@coderox.se>
2019-05-23 18:35:31 +0200

    Clean up

--- source/main.cpp
+++ source/main.cpp
@@ -7,13 +7,6 @@
 #endif
 
 #include <winrt/Windows.Foundation.h>
-// #include <winrt/Windows.Foundation.Collections.h>
-// #include <winrt/Windows.System.h>
-// #include <winrt/Windows.UI.Xaml.h>
-// #include <winrt/Windows.UI.Xaml.Controls.h>
-// #include <winrt/Windows.UI.Xaml.Hosting.h>
-// #include <winrt/Windows.UI.Xaml.Media.h>
-// #include <Windows.UI.Xaml.Hosting.DesktopWindowXamlSource.h>
 
 using namespace winrt;
 using namespace Windows::Foundation;
