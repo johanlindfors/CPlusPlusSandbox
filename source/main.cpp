ea4db1406d15f7fde9b62580c61abe97717856fa
Johan Lindfors // coderox <Johan.Lindfors@coderox.se>
2021-02-08 11:58:09 +0100

    Proper tasks for windows

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,14 @@
 #include <cstdio>
+#include <vector>
 
 int main() {
-    printf("Hello world!\n");
+    std::vector<int> numbers = { 0, 1, 2 , 3 , 4, 5};
+
+    printf("Length of numbers: %zd\n", numbers.size());
+    for(auto iter = numbers.begin(); iter != numbers.end();) {
+        printf("%d", *iter);
+        iter = numbers.erase(iter);
+    }
+    printf("\nLength of numbers: %zd\n", numbers.size());
     return 0;
 }
